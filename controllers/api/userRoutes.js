dbe9ec47cc1b8a7e233030af47d8ceed43cd1f62
ChotanSharma <chotansharma@yahoo.com>
2022-04-07 02:32:20 -0400

    ready with user model and routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-  // Access our User model and run .findAll() method)
-  User.findAll()
+  // Access our User model and run .findAll() method){
+  
+  User.findAll({attributes: { 
+    exclude: ['password'] }
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -15,6 +19,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     }
@@ -34,7 +39,6 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -48,8 +52,6 @@ router.post('/', (req, res) => {
 });
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
     where: {
@@ -89,4 +91,4 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-model.exports = router;
+module.exports = router;
